d2e5d96a17523d755493745b521cf64ab9863c1c
WOLFY9527 <leeyiphang@gmail.com>
2021-05-02 16:05:55 +0800

    atmosphere_mobile

---
+++ code.js
@@ -0,0 +1,75 @@
+onEvent("home", "click", function( ) {
+  setScreen("what");
+});
+onEvent("home1", "click", function( ) {
+  setScreen("what");
+});
+onEvent("home2", "click", function( ) {
+  setScreen("what");
+});
+onEvent("home3", "click", function( ) {
+  setScreen("what");
+});
+onEvent("home4", "click", function( ) {
+  setScreen("what");
+});
+onEvent("home5", "click", function( ) {
+  setScreen("what");
+});
+onEvent("learnb", "click", function( ) {
+  setScreen("what");
+});
+onEvent("orange", "click", function( ) {
+  setScreen("layers");
+});
+onEvent("tro_p", "click", function( ) {
+  setScreen("tro");
+});
+onEvent("tro", "click", function( ) {
+  setScreen("tro1");
+});
+onEvent("str_p", "click", function( ) {
+  setScreen("str");
+});
+onEvent("str", "click", function( ) {
+  setScreen("str1");
+});
+onEvent("mes_p", "click", function( ) {
+  setScreen("mes");
+});
+onEvent("mes", "click", function( ) {
+  setScreen("mes1");
+});
+onEvent("exo_p", "click", function( ) {
+  setScreen("exo");
+});
+onEvent("exo", "click", function( ) {
+  setScreen("exo1");
+});
+onEvent("ino_p", "click", function( ) {
+  setScreen("ion");
+});
+onEvent("ion", "click", function( ) {
+  setScreen("ion1");
+});
+onEvent("the_p", "click", function( ) {
+  setScreen("the");
+});
+onEvent("the", "click", function( ) {
+  setScreen("the1");
+});
+onEvent("tro1", "keypress", function( ) {
+  setScreen("str");
+});
+onEvent("str1", "keypress", function( ) {
+  setScreen("mes");
+});
+onEvent("mes1", "keypress", function( ) {
+  setScreen("ion");
+});
+onEvent("ion1", "keypress", function( ) {
+  setScreen("the");
+});
+onEvent("the1", "keypress", function( ) {
+  setScreen("exo");
+});
